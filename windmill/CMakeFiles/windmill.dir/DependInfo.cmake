e27a99ba8f0a8cc9c2f605f39d381436298b640d
Stark <3038736583@qq.com>
2024-12-05 08:53:56 +0800

    add windmill folder

---
+++ windmill/CMakeFiles/windmill.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/clarencestark/RoboMaster/DX_aimbot/windmill/src/WMFunction.cpp" "windmill/CMakeFiles/windmill.dir/src/WMFunction.cpp.o" "gcc" "windmill/CMakeFiles/windmill.dir/src/WMFunction.cpp.o.d"
+  "/Users/clarencestark/RoboMaster/DX_aimbot/windmill/src/WMIdentify.cpp" "windmill/CMakeFiles/windmill.dir/src/WMIdentify.cpp.o" "gcc" "windmill/CMakeFiles/windmill.dir/src/WMIdentify.cpp.o.d"
+  "/Users/clarencestark/RoboMaster/DX_aimbot/windmill/src/WMInference.cpp" "windmill/CMakeFiles/windmill.dir/src/WMInference.cpp.o" "gcc" "windmill/CMakeFiles/windmill.dir/src/WMInference.cpp.o.d"
+  "/Users/clarencestark/RoboMaster/DX_aimbot/windmill/src/WMPredict.cpp" "windmill/CMakeFiles/windmill.dir/src/WMPredict.cpp.o" "gcc" "windmill/CMakeFiles/windmill.dir/src/WMPredict.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
